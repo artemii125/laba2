dbda1162ea2a76279e778afd1ee4f4398da1afab
artemii125 <168919677+artemii125@users.noreply.github.com>
2025-04-30 10:24:29 +0700

    Update 1.go

--- n1/1.go
+++ n1/1.go
@@ -22,24 +22,26 @@ func main() {
     	var N int
     	fmt.Print("Введите число для определения разности между ним и ближайшим простым: ")
     	fmt.Scan(&N)
-
-    	// Поиск ближайшего простого числа, меньшего или равного N
-    	smaller := N
-    	for !smpl(smaller) {
-        	smaller--
-    	}
-
-    	// Поиск ближайшего простого числа, большего или равного N
-    	larger := N
-    	for !smpl(larger) {
-        	larger++
-    	}
-
-    	// Сравнение расстояний и вывод результата
-    	if N-smaller <= larger-N {
-        	fmt.Println("Разность:", N-smaller)
-    	} else {
-        	fmt.Println("Разность:", larger-N)
-    	}
+	if (N > 2){
+	    	// Поиск ближайшего простого числа, меньшего или равного N
+	    	smaller := N
+	    	for !smpl(smaller) {
+	        	smaller--
+	    	}
+	
+	    	// Поиск ближайшего простого числа, большего или равного N
+	    	larger := N
+	    	for !smpl(larger) {
+	        	larger++
+	    	}
+	
+	    	// Сравнение расстояний и вывод результата
+	    	if N-smaller <= larger-N {
+	        	fmt.Println("Разность:", N-smaller)
+	    	} else {
+	        	fmt.Println("Разность:", larger-N)
+	    	}
+	}
+	fmt.Println(N - 2)
 }
 //

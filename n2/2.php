1ccb99c110434dc9a0eba65ebd30df6df3a08252
artemii125 <168919677+artemii125@users.noreply.github.com>
2025-04-30 12:26:50 +0700

    Update 2.php

--- n2/2.php
+++ n2/2.php
@@ -5,18 +5,18 @@ $cups = array_map('intval', explode(" ", trim(fgets(STDIN))));
 $drunk = $cups[0]; // изначально выпито
 $i = 0;            // счётчик шагов
 $full = 0;         // количество банок после обмена
-
-// пока можно менять
-while ($cups[0] >= $cups[1]) {
-    $i += 1; // шаг: опустошили
-    $full = intdiv($cups[0], $cups[1]); // обмен
-    $i += 1;
-    $empty = $cups[0] % $cups[1]; // остаток
-    $drunk += $full;
-    $cups[0] = $full + $empty; // обновляем запас
+if ($cups[0] > 1){
+    // пока можно менять
+    while ($cups[0] >= $cups[1]) {
+        $i += 1; // шаг: опустошили
+        $full = intdiv($cups[0], $cups[1]); // обмен
+        $i += 1;
+        $empty = $cups[0] % $cups[1]; // остаток
+        $drunk += $full;
+        $cups[0] = $full + $empty; // обновляем запас
+    }
+    
+    $i += 1; // допиваем всё
+    echo "Выпито банок и кол-во итераций: $drunk $i\n";
 }
-
-$i += 1; // допиваем всё
-echo "Выпито банок и кол-во итераций: $drunk $i\n";
 ?>
-//

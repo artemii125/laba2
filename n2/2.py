06156e61b27b8b020ce4d6d71e66a88ec9c433db
artemii125 <168919677+artemii125@users.noreply.github.com>
2025-04-30 12:35:46 +0700

    Update 2.py

--- n2/2.py
+++ n2/2.py
@@ -1,14 +1,14 @@
-cups = list(map(int, input().split())) # [полные, обмен]
+cups = list(map(int, input("Введите кол-во полных банок и эквивалент обмена: ").split())) # [полные, обмен]
 drunk = cups[0] 
 i = 0
 full = 0
-while cups[0] >= cups[1]:
-	i += 1 # шаг опустошения
+if cups[0] > 1:
+    while cups[0] >= cups[1]:
+        i += 1 # шаг опустошения
         full = cups[0] // cups[1] # обмен на полную банку
         i += 1 # шаг обмена
         empty = cups[0] % cups[1] # кол-во пустых банок
         drunk += full # кол-во выпитых
         cups[0] = full + empty
-i += 1 # выпиваем оставшиеся     
-print(drunk, i)
-#
+    i += 1 # выпиваем оставшиеся     
+    print(drunk, i)

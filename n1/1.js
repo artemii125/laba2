13e643ff7d76581837782b8aa55edda80d963525
artemii125 <168919677+artemii125@users.noreply.github.com>
2025-04-30 10:37:11 +0700

    Update 1.js

--- n1/1.js
+++ n1/1.js
@@ -11,17 +11,19 @@ const readline = require('readline');
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 rl.question("Введите число для определения разности между ним и ближайшим простым: ", (input) => {
     const N = parseInt(input);
-
-    // Поиск ближайшего простого числа <= N
-    let smaller = N;
-    while (!smpl(smaller)) smaller--;
-
-    // Поиск ближайшего простого числа >= N
-    let larger = N;
-    while (!smpl(larger)) larger++;
-
-    // Сравнение расстояний и вывод
-    console.log("Разность:", Math.min(N - smaller, larger - N));
+    if (N > 2){
+        // Поиск ближайшего простого числа <= N
+        let smaller = N;
+        while (!smpl(smaller)) smaller--;
+    
+        // Поиск ближайшего простого числа >= N
+        let larger = N;
+        while (!smpl(larger)) larger++;
+    
+        // Сравнение расстояний и вывод
+        console.log("Разность:", Math.min(N - smaller, larger - N));
+    }
+    console.log(N - 2);
     rl.close();
+    
 });
-//
